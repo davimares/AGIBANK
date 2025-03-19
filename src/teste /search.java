98cc686466fc614a9676584e5306b5d7362e3ab1
davimares <92403592+davimares@users.noreply.github.com>
2025-03-18 23:30:02 -0300

    Update search.java

--- src/teste /search.java
+++ src/teste /search.java
@@ -23,12 +23,12 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * Classe de testes automatizados.
  */
-@DisplayName("campo de pesquisa")
+@DisplayName("Campo de pesquisa")
 public class SearchTests {
 
     private static final String BLOG_URL = "https://blog.agibank.com.br/";
     private static final String LUPA_PESQUISA_XPATH = "//*[@id=\"ast-desktop-header\"]/div[1]/div/div/div/div[3]/div[2]/div/div/a";
-    private static final String SEARCH_FIELD_CLASS = "//*[@id=\"search-field\"]";
+    private static final String SEARCH_FIELD_XPATH = "//*[@id=\"search-field\"]";
     private static final String NO_RESULTS_CLASS = "no-results";
     private static final String MENSAGEM_NAO_ENCONTRADO = "Lamentamos, mas nada foi encontrado para sua pesquisa, tente novamente com outras palavras.";
     private static final String PAGE_TITLE_CLASS = "page-title";
@@ -39,21 +39,22 @@ public class SearchTests {
 
     @BeforeEach
     public void setUp() {
-        // Configura o WebDriver
+        // Configuração do WebDriver
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        // Adiciona as opções do Chrome:
+        options.addArguments("--headless", "--disable-gpu", "--no-sandbox", "--disable-extensions", "--start-maximized");
 
         driver = new ChromeDriver(options);
 
-        wait = new WebDriverWait(driver, Duration.ofSeconds(90))
+        // Configuração do WebDriverWait
+        wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(90))
                 .pollingEvery(Duration.ofSeconds(1))
                 .ignoring(NoSuchElementException.class);
     }
 
     @AfterEach
     public void tearDown() {
-        // Fecha o navegador
         if (driver != null) {
             driver.quit();
         }
@@ -67,10 +68,9 @@ public class SearchTests {
         acessarBlog();
         realizarPesquisa(termoDePesquisa);
         rolarPaginaParaResultadosAdicionais();
-        boolean termoEncontrado = verificarResultados(termoDePesquisa);
 
-        // Verifica se o termo de pesquisa foi encontrado
-        assertTrue(termoEncontrado, "Pesquisa não foi encontrada");
+        // Verifica se o termo foi encontrado nos resultados
+        assertTrue(verificarResultados(termoDePesquisa), "O termo de pesquisa não foi encontrado.");
     }
 
     @Test
@@ -81,9 +81,9 @@ public class SearchTests {
         acessarBlog();
         realizarPesquisa(termoDePesquisa);
 
-        // Verifica se a mensagem de não encontrado foi exibida corretamente
-        assertEquals(0, verificarNumeroDeArtigosRetornados());
-        assertTrue(verificarMensagemNaoEncontrado(), "Mensagem de artigo não encontrada");
+        // Verifica se a mensagem de "não encontrado" foi exibida corretamente
+        assertEquals(0, verificarNumeroDeArtigosRetornados(), "Deveriam ser 0 resultados.");
+        assertTrue(verificarMensagemNaoEncontrado(), "A mensagem 'não encontrado' não foi exibida.");
     }
 
     @Test
@@ -94,66 +94,39 @@ public class SearchTests {
         acessarBlog();
         realizarPesquisa(termoDePesquisa);
 
-        // Verifica se o termo injetado retorna mais resultados do que o esperado
-        boolean isInjectionSuccessful = verificarNumeroDeArtigosRetornados() > 0;
-
-        // O teste falha se a injeção for bem-sucedida
-        assertFalse(isInjectionSuccessful, "O campo de pesquisa é vulnerável a SQL Injection.");
+        // Verifica se a SQL Injection foi bem-sucedida
+        assertFalse(verificarNumeroDeArtigosRetornados() > 0, "O campo de pesquisa é vulnerável a SQL Injection.");
     }
 
-    /**
-     * Acessa o blog do Agi.
-     */
     private void acessarBlog() {
         driver.get(BLOG_URL);
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath(LUPA_PESQUISA_XPATH)));
     }
 
-    /**
-     * Realiza a pesquisa no blog utilizando o termo fornecido.
-     *
-     * @param termoDePesquisa O termo a ser pesquisado.
-     */
     private void realizarPesquisa(String termoDePesquisa) {
-
         clicarElemento(By.xpath(LUPA_PESQUISA_XPATH));
-        WebElement campoPesquisa = driver.findElement(By.xpath(SEARCH_FIELD_CLASS));
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(SEARCH_FIELD_CLASS)));
+        WebElement campoPesquisa = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(SEARCH_FIELD_XPATH)));
         campoPesquisa.sendKeys(termoDePesquisa);
         campoPesquisa.submit();
     }
 
-    /**
-     * Clica em um elemento da página identificado pelo By fornecido.
-     *
-     * @param by Localizador do elemento a ser clicado.
-     */
     private void clicarElemento(By by) {
-
-        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(by));
+        element.click();
     }
 
-    /**
-     * Rola a página para carregar mais resultados.
-     */
     private void rolarPaginaParaResultadosAdicionais() {
+        // Rola a página para carregar mais resultados
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(PAGE_TITLE_CLASS)));
-        WheelInput.ScrollOrigin scrollOrigin = WheelInput.ScrollOrigin.fromViewport(10, 10);
         Actions actions = new Actions(driver);
+        WheelInput.ScrollOrigin scrollOrigin = WheelInput.ScrollOrigin.fromViewport(10, 10);
 
         for (int i = 0; i < 250; i++) {
             actions.scrollFromOrigin(scrollOrigin, 0, 1000).perform();
         }
     }
 
-    /**
-     * Verifica se o termo de pesquisa aparece em todos os artigos retornados.
-     *
-     * @param termoDePesquisa O termo a ser verificado.
-     * @return True se o termo foi encontrado em todos os artigos, False caso contrário.
-     */
     private boolean verificarResultados(String termoDePesquisa) {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("article")));
         List<WebElement> artigos = driver.findElements(By.tagName("article"));
         for (WebElement artigo : artigos) {
             if (!verificarTermoNoArtigo(artigo, termoDePesquisa)) {
@@ -163,15 +136,9 @@ public class SearchTests {
         return true;
     }
 
-    /**
-     * Verifica se o termo de pesquisa que não aparece no título aparece no corpo do artigo.
-     *
-     * @param artigo          O artigo a ser verificado.
-     * @param termoDePesquisa O termo a ser pesquisado.
-     * @return True se o termo foi encontrado, False caso contrário.
-     */
     private boolean verificarTermoNoArtigo(WebElement artigo, String termoDePesquisa) {
-        if (artigo.getText().toLowerCase().contains(termoDePesquisa.toLowerCase())) {
+        String textoArtigo = artigo.getText().toLowerCase();
+        if (textoArtigo.contains(termoDePesquisa.toLowerCase())) {
             return true;
         } else {
             artigo.findElement(By.tagName("a")).click();
@@ -182,25 +149,14 @@ public class SearchTests {
         }
     }
 
-    /**
-     * Verifica o número de artigos retornados pela pesquisa.
-     *
-     * @return O número de artigos encontrados.
-     */
     private int verificarNumeroDeArtigosRetornados() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(PAGE_TITLE_CLASS)));
         List<WebElement> artigos = driver.findElements(By.tagName("article"));
         return artigos.size();
     }
 
-    /**
-     * Verifica se a mensagem de "não encontrado" foi exibida corretamente.
-     *
-     * @return True se a mensagem foi exibida, False caso contrário.
-     */
     private boolean verificarMensagemNaoEncontrado() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(PAGE_TITLE_CLASS)));
-        WebElement mensagemNaoEncontrado = driver.findElement(By.className(NO_RESULTS_CLASS));
+        WebElement mensagemNaoEncontrado = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(NO_RESULTS_CLASS)));
         return mensagemNaoEncontrado.getText().contains(MENSAGEM_NAO_ENCONTRADO);
     }
 }
+
